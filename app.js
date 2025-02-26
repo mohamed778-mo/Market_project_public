4ae01e678824a93fe8f55c0ec9c50bc939519c83
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2025-02-26 15:16:10 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const connection = require("./config/connection");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const bodyParser = require("body-parser");
-
+const rateLimit = require('express-rate-limit');
+const express_mongo_sanitize= require('express-mongo-sanitize');
+const xss=require('xss-clean');
+const hpp = require('hpp')
 
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger-output.json");
@@ -17,15 +20,46 @@ const app = express();
 app.use(express.json());
 
 app.use(cors())
+
+const LIMIT = '500kb';
+app.use(bodyParser.json({ limit: LIMIT, extended: true }));
+app.use(bodyParser.urlencoded({ limit: LIMIT, extended: true }));
+app.use(express.json({ limit: LIMIT }));
+
 app.use(cookieParser());
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+const ratelimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, 
+    message: "Too many requests from this IP, please try again later.",
+});
+
+ app.use(ratelimiter); 
+
+ app.use(hpp())
+ app.use(express_mongo_sanitize())
+ app.use(xss())
+
 
 app.use('/app/user',user)
 app.use('/app/admin',admin)
 app.use('/app/payments', payment);
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
+app.all('*', (req, res, next) => {
+  const error = new Error(`Can't find this route ${req.originalUrl}`);
+  next(error); 
+});
+
+
+app.use((err, req, res, next) => {
+  res.status(400).json({
+    success: false,
+    message: err.message 
+  });
+});
 
 connection();
 
